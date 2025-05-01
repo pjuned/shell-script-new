f5210ffe5d99098be9b9efd61aa70767b51d185f
unknown <pjuned.it@gmail.com>
2025-05-01 12:20:31 +0530

    variables

---
+++ variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+person1=ram
+person2=rahim
+
+echo "person1: Hello $person2"
+echo "$person2: good morning $person1"
+echo "$person1: what are you doing "$person2"
+echo "I am learning devops $person1"
