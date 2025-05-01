b743b34cabf8863dd0d44f577789e09fefd202a9
unknown <pjuned.it@gmail.com>
2025-05-01 12:46:23 +0530

    variables

--- variables.sh
+++ variables.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 person1=ram
 person2=rahim
+date=$(date)
 echo "$person1: Hello $person2"
 echo "$person2: good morning $person1"
 echo "$person1: what are you doing $person2"
 echo "$person2:I am learning devops $person1"
+ echo "today date n time is ${date}"
